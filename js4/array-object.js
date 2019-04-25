6afde428d05c47f81eaf3b8a3756e6801baaee30
Evan Wheeler <effyn@gmx.com>
2019-04-25 02:35:46 -0400

    Assignment four

---
+++ js4/array-object.js
@@ -0,0 +1,29 @@
+function arrayObject(array)
+{
+    let result = {
+        strings: [],
+        numbers: [],
+        booleans: []
+    };
+
+    for (let item of array)
+    {
+        switch (typeof item)
+        {
+            case "string":
+                result.strings.push(item);
+                continue;
+            case "number":
+                result.numbers.push(item);
+                continue;
+            case "boolean":
+                result.booleans.push(item);
+        }
+    }
+
+    return result;
+}
+
+let obj = arrayObject([1, 3, true, false, "hello", "world"]);
+
+console.log(obj);
